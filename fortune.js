0762e8e9c21637f1c8c631270b0625c282dcc702
pookie <76092028+howell083@users.noreply.github.com>
2020-12-19 00:41:18 -0600

    add basic 3 part fortunes

--- fortune.js
+++ fortune.js
@@ -1,12 +1,34 @@
 const readline = require("readline");
-console.log('Welcome to the Mixed Fortunes app!');
+let uName;
 
+
+console.log('Welcome to the Mixed Fortunes app!');
+/*
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 rl.question("What is your name ? ", name => {
+    uName = name;
     console.log(`Howdy, ${name}!`);
     rl.close();
 });
+
+*/
+function getDate(){
+    const dateStr = Date.now().toString();
+    const lastTwo = Number.parseInt(dateStr.substr(dateStr.length - 2), 10);
+    return lastTwo;
+};
+function getZodiac(){
+    let sign = Math.ceil(Math.random() * 13);
+    return sign;
+}
+function getFortune(){
+    let advice = 'This is good advice. Promise.';
+    return advice;
+}
+console.log(getDate());
+console.log(getZodiac());
+console.log(getFortune());
